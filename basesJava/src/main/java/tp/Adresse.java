4b4cf6ae320c7d19c5808c760d406dc3f157fd4d
Didier Defrance <didier@d-defrance.fr>
2023-06-16 15:54:39 +0200

    Adresse

---
+++ basesJava/src/main/java/tp/Adresse.java
@@ -0,0 +1,16 @@
+package tp;
+
+public class Adresse {
+	private String num; //ex : "2" ou "2bis"
+	private String rue; 
+	private String codePostal; //ex: "75012"
+	private String ville;
+	
+	/*
+	à faire en Tp :
+	   - get/set
+	   - constructeurs
+	   - toString()
+	   - tester ça depuis MyApp
+	*/
+}
