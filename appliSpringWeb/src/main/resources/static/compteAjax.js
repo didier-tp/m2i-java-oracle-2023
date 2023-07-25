02fad8f2532ddb52a5ee58f2f2cafa53a339b409
Didier Defrance <didier@d-defrance.fr>
2023-07-25 15:58:25 +0200

    s

--- appliSpringWeb/src/main/resources/static/compteAjax.js
+++ appliSpringWeb/src/main/resources/static/compteAjax.js
@@ -1,12 +1,19 @@
 window.onload=function(){
+	(document.getElementById("btnRechercher"))
+	   .addEventListener("click",rechercherComptesSelonSoldeMini);
+}
 	
-	let wsUrl = "./api-bank/compte";
+function rechercherComptesSelonSoldeMini(){	
+	let soldeMini = (document.getElementById("inputSoldeMini")).value;
+	
+	let wsUrl = "./api-bank/compte?soldeMini="+soldeMini;
 	
 	makeAjaxGetRequest(wsUrl,function(responseJson){
 		let comptesJs = JSON.parse(responseJson);
 		console.log("comptesJs="+comptesJs);
 		
 		let bodyElt = document.getElementById("table_body");
+		bodyElt.innerHTML="";//vider le tableau avant de le re-remplir
 		for(let compte of comptesJs){
 			let row = bodyElt.insertRow(-1);
 			(row.insertCell(0)).innerHTML = compte.numero;
