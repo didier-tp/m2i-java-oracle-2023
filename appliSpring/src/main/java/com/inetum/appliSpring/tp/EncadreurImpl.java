a79eab011bfdbc13f3dde1928bf414fa08796b0e
Didier Defrance <didier@d-defrance.fr>
2023-07-31 12:16:41 +0200

    avec @PostConstruct

--- appliSpring/src/main/java/com/inetum/appliSpring/tp/EncadreurImpl.java
+++ appliSpring/src/main/java/com/inetum/appliSpring/tp/EncadreurImpl.java
@@ -2,9 +2,20 @@ package com.inetum.appliSpring.tp;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
 
 @Component
 public class EncadreurImpl implements Encadreur {
+	
+	public EncadreurImpl() {
+		System.out.println("dans constructeur , prefixeur="+this.prefixeur);//null
+	}
+	
+	@PostConstruct
+	public void  initialisation() {
+		System.out.println("dans initialisation préfixée par @PostConstruct , prefixeur="
+	                  +this.prefixeur);//pas null
+	}
 	
 	@Autowired
 	//@Autowired (auto-laison) demande à Spring de automatiquement 
