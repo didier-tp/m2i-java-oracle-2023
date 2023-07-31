351a7a502dcca0e90264c0c06def542bf3d2aefe
Didier Defrance <didier@d-defrance.fr>
2023-07-31 10:52:27 +0200

    s

--- appliSpring/src/main/java/com/inetum/appliSpring/AppliSpringApplication.java
+++ appliSpring/src/main/java/com/inetum/appliSpring/AppliSpringApplication.java
@@ -8,14 +8,20 @@ public class AppliSpringApplication {
 
 	public static void main(String[] args) {
 		
-		
 		AnnotationConfigApplicationContext springContext = new
 				AnnotationConfigApplicationContext(MySpringConfig.class);
+		//NB: dès la création de springContext via une configuration xml ou java
+		//toutes les instances des composants spring nécessaires sont créées et initailisées en 
+		//mémoire
+		
+		//la méthode .getBean() permet d'obtenir une référence sur
+		//un des composants pris en charge par spring
 		Prefixeur prefixeur = springContext.getBean(Prefixeur.class);
+		
 		System.out.println("appliSpring démarrée");
 		System.out.println("chaine prefixee=" + prefixeur.prefixer("lundi"));
 		
-		springContext.close();
+		springContext.close();//arrêt/fermeture de Spring
 	}
 
 }
