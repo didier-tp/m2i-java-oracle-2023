622c4559b1bd19577351e7c63dc8388ae3b925ba
Didier Defrance <didier@d-defrance.fr>
2023-07-31 10:15:20 +0200

    appliSpring

---
+++ appliSpring/src/main/java/com/inetum/appliSpring/AppliSpringApplication.java
@@ -0,0 +1,13 @@
+package com.inetum.appliSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliSpringApplication.class, args);
+	}
+
+}
---
+++ appliSpring/src/test/java/com/inetum/appliSpring/AppliSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.appliSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppliSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
