215c3d61e1b32dbf7ac23b77dc0fec0621735560
Didier Defrance <didier@d-defrance.fr>
2023-08-01 10:14:12 +0200

    s

---
+++ appliSpring/src/main/java/com/inetum/appliSpring/DataSourceConfig.java
@@ -0,0 +1,37 @@
+package com.inetum.appliSpring;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class DataSourceConfig {
+
+	@Value("${jdbc.driver}")
+	private String jdbcDriver;
+	
+	@Value("${db.url}")
+	private String dbUrl;
+	
+	@Value("${db.username}")
+	private String dbUsername;
+	
+	@Value("${db.password}")
+	private String dbPassword;
+	
+	@Bean(name="myDataSource")
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(jdbcDriver);
+		dataSource.setUrl(dbUrl);
+		dataSource.setUsername(dbUsername);
+		dataSource.setPassword(dbPassword);
+		return dataSource;
+	}
+	
+}
