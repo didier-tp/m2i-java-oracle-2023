2a5e3b1afb8ad4af3853ab0f32ee1bcea76e4b45
Didier Defrance <didier@d-defrance.fr>
2023-08-04 11:18:05 +0200

    s

---
+++ appliSpringWeb3/src/main/java/com/inetum/appliSpringWeb/dto/ApiError.java
@@ -0,0 +1,32 @@
+package com.inetum.appliSpringWeb.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @NoArgsConstructor
+public class ApiError {
+
+	private HttpStatus status;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+	private LocalDateTime timestamp;
+	
+	private String message;
+	//private String details;
+
+	public ApiError(HttpStatus status, String message) {
+		super();
+		this.status = status;
+		this.message = message;
+		this.timestamp = LocalDateTime.now();
+	}
+	
+}
