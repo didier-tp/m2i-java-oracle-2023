7ad7cf0844d0a0e917c81cd44bd87fd3d291bdb3
Didier Defrance <didier@d-defrance.fr>
2023-08-21 15:26:10 +0200

    s

--- appliSpringWeb4/src/main/java/com/inetum/appliSpringWeb/rest/DeviseRestCtrl.java
+++ appliSpringWeb4/src/main/java/com/inetum/appliSpringWeb/rest/DeviseRestCtrl.java
@@ -1,5 +1,6 @@
 package com.inetum.appliSpringWeb.rest;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ public class DeviseRestCtrl {
 	//exemple de fin d'URL: ./api-devise/devise/USD
 	//@RequestMapping(value="/{codeDevise}" , method=RequestMethod.GET)
 	@GetMapping("/{codeDevise}" )
+	@PreAuthorize("hasRole('ADMIN')")
 	public Devise getDeviseByCode(@PathVariable("codeDevise") String code) {
 		Devise devise = new Devise(code,"devise",1.1);
 		//devise.setChange(1.01);
