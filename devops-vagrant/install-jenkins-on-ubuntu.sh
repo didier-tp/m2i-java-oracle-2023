4db3260c545c7af9aa054e76c76786ec76c98bf2
Didier Defrance <didier@d-defrance.fr>
2023-08-24 10:37:16 +0200

    s

--- devops-vagrant/install-jenkins-on-ubuntu.sh
+++ devops-vagrant/install-jenkins-on-ubuntu.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \ /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
