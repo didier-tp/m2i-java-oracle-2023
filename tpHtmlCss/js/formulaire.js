52da6d12e13b493c85719bccf0d52231bf9f81c2
Didier Defrance <didier@d-defrance.fr>
2023-08-25 10:38:35 +0200

    s

--- tpHtmlCss/js/formulaire.js
+++ tpHtmlCss/js/formulaire.js
@@ -1,4 +1,16 @@
 function enregistrer(){
+    let inputNom = document.querySelector("input[name='nom'] ");
+    let valeurSaisieNom = inputNom.value;
+
+    let inputPrenom = document.querySelector("input[name='prenom'] ");
+    let valeurSaisiePrenom = inputPrenom.value;
+
+    let personneJs = {
+        nom : valeurSaisieNom ,
+        prenom : valeurSaisiePrenom 
+    }
+    let presonneAsJsonString  = JSON.stringify(personneJs);
+
     let zoneMsg = document.getElementById("msg");
-    zoneMsg.innerHTML="message1";
+    zoneMsg.innerHTML=presonneAsJsonString;
 }
